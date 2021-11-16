18d5c783752ae0ef86cd6f4cbcd7108b79a2a07d
CHEW TZE YANG RP <19037802@myrp.edu.sg>
2021-11-16 22:44:22 +0800

    update menu-items to navigate with react router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import "./App.css";
 import HomePage from "./pages/homepage/homepage.component";
+import { Routes, Route } from "react-router-dom";
 
+const HatsPage = () => (
+  <div>
+    <h1>HATS PAGE</h1>
+  </div>
+);
 function App() {
   return (
     <div>
-      <HomePage />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/shop/hats" element={<HatsPage />} />
+      </Routes>
     </div>
   );
 }
